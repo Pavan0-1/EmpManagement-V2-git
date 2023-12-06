0aa924922363b470d21f4fae35814fc5f2ff3bcd
Sri Rajendra Turaga <Rajendratm120@gmail.com>
2023-12-06 20:34:57 +0530

    Delete foodapp directory

--- foodapp/Recipe/Recipe/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿function spinner(flag) {
-    if (flag) {
-        $('.spinner-bg').removeClass('d-none')
-    }
-    else {
-        $('.spinner-bg').removeClass('d-none').addClass('d-none')
-    }
-}
